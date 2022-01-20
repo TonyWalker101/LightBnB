4c7f0dde3c5b4712728d7df5c36ab88fffbcc647
Tony <awalker780@gmail.com>
2022-01-19 20:00:35 -0500

    Queries for LighthouseBnB SELECT

---
+++ 1_queries/04_all_my_reservations.sql
@@ -0,0 +1,8 @@
+SELECT reservations.id, properties.title, start_date, cost_per_night, AVG(rating)
+  FROM properties
+    JOIN reservations ON properties.id = reservations.property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = 1
+  GROUP BY reservations.id, properties.title, cost_per_night
+  ORDER BY start_date
+  LIMIT 10;
